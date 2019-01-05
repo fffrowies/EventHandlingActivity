ef5017b53d0c45967724d15c69eb78cf0e1d09b1
fffrowies <f3rnandorowies.saxo@gmail.com>
2019-01-05 20:50:26 -0300

    Handling Event inside Activity using Event Listener Interface

--- app/src/main/java/com/fffrowies/eventhandlingactivity/MainActivity.java
+++ app/src/main/java/com/fffrowies/eventhandlingactivity/MainActivity.java
@@ -1,12 +1,16 @@
 package com.fffrowies.eventhandlingactivity;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     private RelativeLayout relativeLayout;
 
     @Override
@@ -18,5 +22,22 @@ public class MainActivity extends AppCompatActivity {
 
         Button button1 = findViewById(R.id.button1);
         Button button2 = findViewById(R.id.button2);
+
+        button1.setOnClickListener(MainActivity.this);
+        button2.setOnClickListener(MainActivity.this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
+                Log.i(TAG, "Color changed to Green");
+                relativeLayout.setBackgroundColor(Color.GREEN);
+                break;
+            case R.id.button2:
+                Log.i(TAG, "Color changed to Blue");
+                relativeLayout.setBackgroundColor(Color.BLUE);
+                break;
+        }
     }
 }
