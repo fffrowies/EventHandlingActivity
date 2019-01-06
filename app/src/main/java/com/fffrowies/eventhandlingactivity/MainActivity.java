d4278f26faf9b2c7955b920da2c5664c774f7479
fffrowies <f3rnandorowies.saxo@gmail.com>
2019-01-05 21:19:25 -0300

    Handling Event using onClick attribute

--- app/src/main/java/com/fffrowies/eventhandlingactivity/MainActivity.java
+++ app/src/main/java/com/fffrowies/eventhandlingactivity/MainActivity.java
@@ -3,38 +3,27 @@ package com.fffrowies.eventhandlingactivity;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final RelativeLayout relativeLayout = findViewById(R.id.relativeLayout);
+        relativeLayout = findViewById(R.id.relativeLayout);
+    }
 
-        Button button1 = findViewById(R.id.button1);
-        Button button2 = findViewById(R.id.button2);
+    public void changeToBlack(View view) {
+        relativeLayout.setBackgroundColor(Color.BLACK);
+    }
 
-        // new View.OnClickListener() {...} - It refers to an Object that implements an interface.
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                relativeLayout.setBackgroundColor(Color.YELLOW);
-            }
-        });
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                relativeLayout.setBackgroundColor(Color.RED);
-            }
-        });
+    public void changeToGray(View view) {
+        relativeLayout.setBackgroundColor(Color.GRAY);
     }
 }
