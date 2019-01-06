b1dd358bc2ad9712c72c047f069d09c672517168
fffrowies <f3rnandorowies.saxo@gmail.com>
2019-01-05 21:09:07 -0300

    Alternate method to handling an Event inside Activity

--- app/src/main/java/com/fffrowies/eventhandlingactivity/MainActivity.java
+++ app/src/main/java/com/fffrowies/eventhandlingactivity/MainActivity.java
@@ -8,36 +8,33 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    private RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        relativeLayout = findViewById(R.id.relativeLayout);
+        final RelativeLayout relativeLayout = findViewById(R.id.relativeLayout);
 
         Button button1 = findViewById(R.id.button1);
         Button button2 = findViewById(R.id.button2);
 
-        button1.setOnClickListener(MainActivity.this);
-        button2.setOnClickListener(MainActivity.this);
-    }
+        // new View.OnClickListener() {...} - It refers to an Object that implements an interface.
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                relativeLayout.setBackgroundColor(Color.YELLOW);
+            }
+        });
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.button1:
-                Log.i(TAG, "Color changed to Green");
-                relativeLayout.setBackgroundColor(Color.GREEN);
-                break;
-            case R.id.button2:
-                Log.i(TAG, "Color changed to Blue");
-                relativeLayout.setBackgroundColor(Color.BLUE);
-                break;
-        }
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                relativeLayout.setBackgroundColor(Color.RED);
+            }
+        });
     }
 }
